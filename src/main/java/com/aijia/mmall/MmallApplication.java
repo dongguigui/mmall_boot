c9f063ee3e0479d67aa3149add77e18d50b6b048
aijiale <1556880760@qq.com>
2018-03-27 21:28:55 +0800

    usingt springboot shopping mall

---
+++ src/main/java/com/aijia/mmall/MmallApplication.java
@@ -0,0 +1,12 @@
+package com.aijia.mmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmallApplication.class, args);
+	}
+}
